79c10a909e761ebe4079911f3d3f4f98ca65a554
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-13 18:25:26 +0000

    refactor(ConsoleStyle): add the `update()` `set()` and use typescript to read the style names.

--- src/lib/console-style.class.ts
+++ src/lib/console-style.class.ts
@@ -4,23 +4,38 @@
  * @abstract
  * @class Console
  */
-export class ConsoleStyle {
-  public get style() {
-    return this.#style;
-  }
-
-  #style: { [index: string]: string } = {
+export class ConsoleStyle<Style extends object = object> {
+  public static style = {
     bold: '\x1b[1m',
     default: '\x1b[0m',
     faint: '\x1b[2m',
     italic: ''
-  };
+  };;
 
-  constructor(style: {[index: string]: string}) {
-    this.#style = { ...this.#style, ...style};
+  public get style(): Style & typeof ConsoleStyle.style {
+    return this.#style as any;
   }
 
-  public get(name: keyof typeof this.style) {
-    return this.#style[name];
+  #style: Style & {[name: string]: string};
+
+  constructor(style?: Style) {
+    this.#style = { ...ConsoleStyle.style, ...style || {} } as any;
+  }
+
+  public get<Name extends PropertyKey>(name: Name | keyof Style | keyof typeof this.style): string {
+    return (this.#style as any)[name];
+  }
+
+  public set<Name extends PropertyKey>(
+    name: Name | keyof Style | keyof typeof this.style,
+    style: string
+  ): this {
+    Object.assign(this.#style, {[name]: style});
+    return this;
+  }
+
+  public update(style: {[name: string]: string } | Partial<Style | typeof this.style>): this {
+    this.#style = { ...this.#style, ...style as object };
+    return this;
   }
 }
